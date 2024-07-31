e6c0a73d764bb610819eb9d1a85ea67361058648
Nicolas Cabezas <nicolas.cabezas@udla.edu.ec>
2024-07-31 18:34:27 -0500

    ok

--- index.js
+++ index.js
@@ -7,24 +7,26 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 bot.onText(/\/puerta/, async (msg) => {
     const chatId = msg.chat.id;
+    //get devices list
+    const devices = await getDevices();
+    inline_keyboard = devices.map(device => {
+        return [{ text: device.name, callback_data: `open ${device.id}` }];
+    });
     bot.sendMessage(chatId, 'Abrir puerta', {
         reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: 'Abrir puerta', callback_data: 'open' }
-                ]
-            ]
+            inline_keyboard: inline_keyboard
         }
     });
 });
 
 bot.on('callback_query', async (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
-    if (callbackQuery.data === 'open') {
+    if (callbackQuery.data.includes('open')) {
         const originalMessage = callbackQuery.message;
         const message = await bot.sendMessage(chatId, 'Solicitando apertura de puerta...');
         try {
-            const result = await openDoor();
+            device_id = callbackQuery.data.split(' ')[1];
+            const result = await openDoor(device_id);
             console.log(result);
             if (result == 'Success') {
                 //delete original message
@@ -40,12 +42,26 @@ bot.on('callback_query', async (callbackQuery) => {
         }
     }
 });
-
-async function openDoor() {
+// Function to open the door, query to the API on /api/sendAccessRequest
+async function openDoor(device_id) {
     console.log('Opening door');
     try {
-        const response = await axios.post('https://access-core.lerolero.com.ec/api/sendAccessRequest');
+        const response = await axios.post('https://access-core.lerolero.com.ec/api/sendAccessRequest', {
+            device_id: device_id
+        }
+        );
         return "Success";
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error');
+    }
+}
+
+//Function to get devices list, query to the API on /api/devices
+async function getDevices() {
+    try {
+        const response = await axios.get('https://access-core.lerolero.com.ec/api/devices');
+        return response.data;
     } catch (error) {
         console.error(error);
         throw new Error('Error');
